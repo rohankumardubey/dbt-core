345c2b227120f83bae4ef993c8329c5a623499ee
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-03-31 17:29:22 -0500

    rename refresh macro in api level

--- core/dbt/include/global_project/macros/db_api/materialized_view/refresh.sql
+++ core/dbt/include/global_project/macros/db_api/materialized_view/refresh.sql
@@ -1,8 +1,8 @@
-{% macro db_api__materialized_view__refresh(relation) %}
+{% macro db_api__materialized_view_refresh(relation) %}
     {{ adapter.dispatch('db_api__materialized_view__refresh', 'dbt')(relation) }}
 {% endmacro %}
 
-{% macro default__db_api_refresh_materialized_view(relation) -%}
+{% macro default__db_api__materialized_view_refresh(relation) -%}
 
     {{ exceptions.raise_not_implemented(
     'db_api__materialized_view__refresh not implemented for adapter '+adapter.type()) }}
