caad12cbd942695a76ef750c53fa234e83193fed
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-03-31 17:31:26 -0500

    align names between postgres and default to same convention

--- core/dbt/include/global_project/macros/db_api/materialized_view/create.sql
+++ core/dbt/include/global_project/macros/db_api/materialized_view/create.sql
@@ -1,10 +1,10 @@
-{% macro db_api__create_materialized_view_as(relation, sql) %}
-    {{ adapter.dispatch('db_api__create_materialized_view_as', 'dbt')(relation, sql) }}
+{% macro db_api__materialized_view__create(relation, sql) %}
+    {{ adapter.dispatch('db_api__materialized_view__create', 'dbt')(relation, sql) }}
 {% endmacro %}
 
-{% macro default_db_api__create_materialized_view_as(relation, sql) -%}
+{% macro default_db_api__materialized_view__create(relation, sql) -%}
 
     {{ exceptions.raise_not_implemented(
-    'db_api__create_materialized_view_as macro not implemented for adapter '+adapter.type()) }}
+    'db_api__materialized_view__create macro not implemented for adapter '+adapter.type()) }}
 
 {% endmacro %}
--- core/dbt/include/global_project/macros/db_api/materialized_view/refresh.sql
+++ core/dbt/include/global_project/macros/db_api/materialized_view/refresh.sql
@@ -1,8 +1,8 @@
-{% macro db_api__materialized_view_refresh(relation) %}
+{% macro db_api__materialized_view__refresh(relation) %}
     {{ adapter.dispatch('db_api__materialized_view__refresh', 'dbt')(relation) }}
 {% endmacro %}
 
-{% macro default__db_api__materialized_view_refresh(relation) -%}
+{% macro default__db_api__materialized_view__refresh(relation) -%}
 
     {{ exceptions.raise_not_implemented(
     'db_api__materialized_view__refresh not implemented for adapter '+adapter.type()) }}
