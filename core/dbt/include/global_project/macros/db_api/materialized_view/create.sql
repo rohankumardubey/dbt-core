0252ea3d186af6d5d67b640a809419c355937503
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-03-31 16:35:01 -0500

    add strategies to condtional in place of old macros

--- core/dbt/include/global_project/macros/db_api/materialized_view/create.sql
+++ core/dbt/include/global_project/macros/db_api/materialized_view/create.sql
@@ -1,10 +1,10 @@
 {% macro db_api__create_materialized_view_as(relation, sql) %}
-    {{ adapter.dispatch('create_materialized_view_as', 'dbt')(relation, sql) }}
+    {{ adapter.dispatch('db_api__create_materialized_view_as', 'dbt')(relation, sql) }}
 {% endmacro %}
 
 {% macro default_db_api__create_materialized_view_as(relation, sql) -%}
 
     {{ exceptions.raise_not_implemented(
-    'create_materialized_view_as macro not implemented for adapter '+adapter.type()) }}
+    'db_api__create_materialized_view_as macro not implemented for adapter '+adapter.type()) }}
 
 {% endmacro %}
--- core/dbt/include/global_project/macros/db_api/materialized_view/refresh.sql
+++ core/dbt/include/global_project/macros/db_api/materialized_view/refresh.sql
@@ -1,10 +1,10 @@
 {% macro db_api__materialized_view__refresh(relation) %}
-    {{ adapter.dispatch('refresh_materialized_view', 'dbt')(relation) }}
+    {{ adapter.dispatch('db_api__materialized_view__refresh', 'dbt')(relation) }}
 {% endmacro %}
 
 {% macro default__db_api_refresh_materialized_view(relation) -%}
 
     {{ exceptions.raise_not_implemented(
-    'refresh_materialized_view not implemented for adapter '+adapter.type()) }}
+    'db_api__materialized_view__refresh not implemented for adapter '+adapter.type()) }}
 
 {% endmacro %}
