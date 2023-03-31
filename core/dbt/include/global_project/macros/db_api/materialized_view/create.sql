d6eee2d0f261e0d36dec845237f46c448dfc8b44
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-03-31 17:33:31 -0500

    align names between postgres and default to same convention

--- core/dbt/include/global_project/macros/db_api/materialized_view/create.sql
+++ core/dbt/include/global_project/macros/db_api/materialized_view/create.sql
@@ -2,7 +2,7 @@
     {{ adapter.dispatch('db_api__materialized_view__create', 'dbt')(relation, sql) }}
 {% endmacro %}
 
-{% macro default_db_api__materialized_view__create(relation, sql) -%}
+{% macro default__db_api__materialized_view__create(relation, sql) -%}
 
     {{ exceptions.raise_not_implemented(
     'db_api__materialized_view__create macro not implemented for adapter '+adapter.type()) }}
