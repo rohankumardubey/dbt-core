e9c423e1c9112e1e1ba19425a08ec7b5152fec24
Matthew McKnight <matthew.mcknight@dbtlabs.com>
2023-04-03 10:48:08 -0500

    pull adapter rename into strategy, add backup_relation as optional arg

--- core/dbt/include/global_project/macros/strategies/materialized_view/full_refresh.sql
+++ core/dbt/include/global_project/macros/strategies/materialized_view/full_refresh.sql
@@ -1,4 +1,12 @@
-{% macro strategy__materialized_view__full_refresh(relation, sql) %}
+{% macro strategy__materialized_view__full__refresh(relation, sql, backup_relation=None) %}
+    {{ adapter.dispatch('strategy__materialized_view__full__refresh', 'dbt')(relation, sql, backup_relation) }}
+{% endmacro %}
+
+
+{% macro default__strategy__materialized_view__full__refresh(relation, sql, backup_relation=None) %}
+    {% if backup_relation %}
+        {{ adapter.rename_relation(target_relation, backup_relation) }}
+    {% endif %}
     {{ drop_relation_if_exists(relation) }}
     {{ db_api__materialized_view__create(relation, sql) }}
 {% endmacro %}
