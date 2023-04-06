47b915ddc1b19c5204564bd590cbe38beaf9c198
Chenyu Li <chenyu.li@dbtlabs.com>
2023-04-06 15:14:41 -0700

    add removal of node to make dbt-server compile query work

--- core/dbt/parser/sql.py
+++ core/dbt/parser/sql.py
@@ -1,14 +1,11 @@
 import os
 from dataclasses import dataclass
-from typing import Iterable
 
 from dbt.contracts.graph.manifest import SourceFile
-from dbt.contracts.graph.nodes import SqlNode, Macro
-from dbt.contracts.graph.unparsed import UnparsedMacro
+from dbt.contracts.graph.nodes import SqlNode
 from dbt.exceptions import DbtInternalError
 from dbt.node_types import NodeType
 from dbt.parser.base import SimpleSQLParser
-from dbt.parser.macros import MacroParser
 from dbt.parser.search import FileBlock
 
 
@@ -46,17 +43,3 @@ class SqlBlockParser(SimpleSQLParser[SqlNode]):
         source_file = SourceFile.remote(sql, self.project.project_name, "sql")
         contents = SqlBlock(block_name=name, file=source_file)
         return self.parse_node(contents)
-
-
-class SqlMacroParser(MacroParser):
-    def parse_remote(self, contents) -> Iterable[Macro]:
-        base = UnparsedMacro(
-            path="from remote system",
-            original_file_path="from remote system",
-            package_name=self.project.project_name,
-            raw_code=contents,
-            language="sql",
-            resource_type=NodeType.Macro,
-        )
-        for node in self.parse_unparsed_macros(base):
-            yield node
--- core/dbt/task/compile.py
+++ core/dbt/task/compile.py
@@ -40,6 +40,18 @@ class CompileRunner(BaseRunner):
 
 
 class CompileTask(GraphRunnableTask):
+    # We add a new inline node to the manifest during initialization
+    # it should be removed before the task is complete
+    _inline_node_id = None
+
+    # TODO remove when stu's PR about skip adapter cache is merged
+    def before_run(self, adapter, selected_uids: AbstractSet[str]):
+        if bool(getattr(self.args, "inline", None)):
+            # don't populate adapter cache when doing inline queries
+            pass
+        else:
+            super().before_run(adapter, selected_uids)
+
     def raise_on_first_error(self):
         return True
 
@@ -129,8 +141,16 @@ class CompileTask(GraphRunnableTask):
             )
             sql_node = block_parser.parse_remote(self.args.inline, "inline_query")
             process_node(self.config, self.manifest, sql_node)
+            # keep track of the node added to the manifest
+            self._inline_node_id = sql_node.unique_id
 
         super()._runtime_initialize()
+
+    def after_run(self, adapter, results):
+        # remove inline node from manifest
+        if self._inline_node_id:
+            self.manifest.nodes.pop(self._inline_node_id)
+        super().after_run(adapter, results)
 
     def _handle_result(self, result):
         super()._handle_result(result)
